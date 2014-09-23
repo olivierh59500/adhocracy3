d975e3b1434f3aa1ae163fc5e0118a069c7d3b21
Matthias Fischmann <mf@zerobuzz.net>
2014-09-23 16:28:13 +0200

    Add failing integration test.
    For now, the test just times out because the WebSocket service
    throws an exception asynchronously.

---
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/WebSocket/WebSocketIg.ts
@@ -0,0 +1,25 @@
+/// <reference path="../../../lib/DefinitelyTyped/jasmine/jasmine.d.ts"/>
+/// <reference path="../../../lib/DefinitelyTyped/modernizr/modernizr.d.ts"/>
+
+import modernizr = require("modernizr");
+import Config = require("../Config/Config");
+import WebSocket = require("../WebSocket/WebSocket");
+
+export var register = (angular, config, meta_api) => {
+
+    describe("WebSocket", () => {
+        var ws : WebSocket.IService;
+
+        beforeEach(() => {
+            ws = WebSocket.factory(<any>modernizr, config);
+        });
+
+        it("handles basic subscription to /adhocracy nicely.", (done) => {
+            ws.register("/adhocracy", (event) => {
+                done();
+            });
+            // this test will throw an async exception and time out if
+            // an error response is sent from backend.
+        });
+    });
+};
