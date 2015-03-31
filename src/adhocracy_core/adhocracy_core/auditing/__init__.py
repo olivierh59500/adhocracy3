67d5ebcbe0b485edd0e1770f974b10fa79b2e52e
Pierre Allix <pierre.allix@liqd.de>
2015-03-31 17:50:07 +0200

    Use of pyramid hooks to log data changes

--- src/adhocracy_core/adhocracy_core/auditing/__init__.py
+++ src/adhocracy_core/adhocracy_core/auditing/__init__.py
@@ -2,22 +2,31 @@
 import transaction
 import json
 
+from pyramid.traversal import resource_path
 from substanced.util import get_auditlog
 from BTrees.OOBTree import OOBTree
 from datetime import datetime
 from logging import getLogger
 from collections import namedtuple
+from adhocracy_core.utils import get_user
+from adhocracy_core.sheets.principal import IUserBasic
+from adhocracy_core.utils import get_sheet_field
+from pyramid_zodbconn import get_connection
+
 
 logger = getLogger(__name__)
 
-AuditEntry = namedtuple('AuditEntry', ['name', 'oid', 'payload'])
+AuditEntry = namedtuple('AuditEntry', ['name', 'payload'])
+
+RESOURCE_CREATED = 'resourceCreated'
+RESOURCE_MODIFIED = 'resourceModified'
 
 
 class AuditLog(OOBTree):
 
     """An Auditlog composed of entries."""
 
-    def add(self, name, oid, **kw):
+    def add(self, name, **kw):
         """ Add a record the audit log.
 
         ``_name`` should be the event name,
@@ -25,7 +34,7 @@ class AuditLog(OOBTree):
         json-serializable dictionary.
         """
         payload = json.dumps(kw)
-        self[datetime.utcnow()] = AuditEntry(name, oid, payload)
+        self[datetime.utcnow()] = AuditEntry(name, payload)
 
 
 def _set_auditlog(context):
@@ -51,7 +60,7 @@ def _get_auditlog(context):
     return auditlog
 
 
-def log_auditevent(context, name, oid=None, **kw):
+def log_auditevent(context, name, **kw):
     """Add a an audit entry to the audit database.
 
     The audit database is created if missing. If the zodbconn.uri.audit
@@ -59,4 +68,35 @@ def log_auditevent(context, name, oid=None, **kw):
     """
     auditlog = _get_auditlog(context)
     if auditlog is not None:
-        auditlog.add(name, oid, **kw)
+        auditlog.add(name, **kw)
+
+
+def audit_changes_callback(request, response):
+    """Add audit entries to the auditlog when the data is changed."""
+    registry = request.registry
+    changelog_metadata = registry.changelog.values()
+    if len(changelog_metadata) == 0:
+        return
+
+    connection = get_connection(request)
+    root = connection.root()
+    user = get_user(request)
+    user_name = get_sheet_field(user, IUserBasic, 'name')
+    user_path = resource_path(user)
+
+    for meta in changelog_metadata:
+        path = resource_path(meta.resource)
+        if meta.created:
+            log_auditevent(root,
+                           RESOURCE_CREATED,
+                           resource_path=path,
+                           user_name=user_name,
+                           user_path=user_path)
+        elif meta.modified:
+            log_auditevent(root,
+                           RESOURCE_MODIFIED,
+                           resource_path=path,
+                           user_name=user_name,
+                           user_path=user_path)
+        # else: log visibility changes?
+        transaction.commit()
