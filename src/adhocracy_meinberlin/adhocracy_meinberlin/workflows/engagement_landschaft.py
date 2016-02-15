e47ecedcd5b17afd89f2f8390e712496e3c2ae68
John <john.schmid@liqd.de>
2016-02-15 11:31:05 +0100

    add workflow module

---
+++ src/adhocracy_meinberlin/adhocracy_meinberlin/workflows/engagement_landschaft.py
@@ -0,0 +1,8 @@
+"""Example workflow."""
+from adhocracy_core.workflows import add_workflow
+from adhocracy_core.workflows.standard import standard_meta
+
+
+def includeme(config):
+    """Add workflow."""
+    add_workflow(config.registry, standard_meta, 'engagement_landschaft')
