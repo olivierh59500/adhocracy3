2ebae53203d1dabbd2b8a0147fc95ce12403548c
John <john.schmid@liqd.de>
2016-03-02 14:29:40 +0100

    buergerhaushalt: rm Context
    context has no other function other than its header, which is now a CustomHeader.

--- src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Buergerhaushalt/Module.ts
+++ src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Buergerhaushalt/Module.ts
@@ -1,4 +1,3 @@
-import * as AdhMeinberlinBuergerhaushaltContextModule from "./Context/Module";
 import * as AdhMeinberlinBuergerhaushaltProcessModule from "./Process/Module";
 import * as AdhMeinberlinBuergerhaushaltWorkbenchModule from "./Workbench/Module";
 
@@ -6,13 +5,11 @@ import * as AdhMeinberlinBuergerhaushaltWorkbenchModule from "./Workbench/Module
 export var moduleName = "adhMeinberlinBuergerhaushalt";
 
 export var register = (angular) => {
-    AdhMeinberlinBuergerhaushaltContextModule.register(angular);
     AdhMeinberlinBuergerhaushaltProcessModule.register(angular);
     AdhMeinberlinBuergerhaushaltWorkbenchModule.register(angular);
 
     angular
         .module(moduleName, [
-            AdhMeinberlinBuergerhaushaltContextModule.moduleName,
             AdhMeinberlinBuergerhaushaltProcessModule.moduleName,
             AdhMeinberlinBuergerhaushaltWorkbenchModule.moduleName
         ]);
