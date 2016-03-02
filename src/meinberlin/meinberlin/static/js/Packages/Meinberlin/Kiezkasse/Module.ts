1c169351ebe34de8e70b0af7ae24dc43bb40827e
John <john.schmid@liqd.de>
2016-03-02 14:32:11 +0100

    kiezkasse: rm Context
    the context module has no other function other than its header, which is now a CustomHeader.

--- src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Kiezkasse/Module.ts
+++ src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Kiezkasse/Module.ts
@@ -1,4 +1,3 @@
-import * as AdhMeinberlinKiezkasseContextModule from "./Context/Module";
 import * as AdhMeinberlinKiezkasseProcessModule from "./Process/Module";
 import * as AdhMeinberlinKiezkasseWorkbenchModule from "./Workbench/Module";
 
@@ -6,13 +5,11 @@ import * as AdhMeinberlinKiezkasseWorkbenchModule from "./Workbench/Module";
 export var moduleName = "adhMeinberlinKiezkasse";
 
 export var register = (angular) => {
-    AdhMeinberlinKiezkasseContextModule.register(angular);
     AdhMeinberlinKiezkasseProcessModule.register(angular);
     AdhMeinberlinKiezkasseWorkbenchModule.register(angular);
 
     angular
         .module(moduleName, [
-            AdhMeinberlinKiezkasseContextModule.moduleName,
             AdhMeinberlinKiezkasseProcessModule.moduleName,
             AdhMeinberlinKiezkasseWorkbenchModule.moduleName
         ]);
