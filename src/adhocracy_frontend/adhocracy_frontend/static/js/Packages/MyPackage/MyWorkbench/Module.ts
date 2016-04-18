102e7a86cd88fbf848a01daed216c4ec7649f988
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-04-18 12:09:11 +0200

    add workbench directive boilerplate

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/MyPackage/MyWorkbench/Module.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/MyPackage/MyWorkbench/Module.ts
@@ -1,7 +1,19 @@
+import * as AdhMovingColumnsModule from "../../MovingColumns/Module";
+import * as AdhTopLevelStateModule from "../../TopLevelState/Module";
+
+import * as MyWorkbench from "./MyWorkbench";
+
 export var moduleName = "adhMyPackageMyWorkbench";
 
 
 export var register = (angular) => {
     angular
-        .module(moduleName, []);
+        .module(moduleName, [
+            AdhMovingColumnsModule.moduleName,
+            AdhTopLevelStateModule.moduleName
+        ])
+        .directive("adhMyWorkbench", ["adhConfig", "adhTopLevelState", MyWorkbench.workbenchDirective])
+        .directive("adhMyWorkbenchProposalDetailColumn", ["adhConfig", MyWorkbench.proposalDetailColumnDirective])
+        .directive("adhMyWorkbenchProposalCreateColumn", ["adhConfig", MyWorkbench.proposalCreateColumnDirective])
+        .directive("adhMyWorkbenchProposalListingColumn", ["adhConfig", MyWorkbench.proposalListingColumnDirective]);
 };
---
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/MyPackage/MyWorkbench/MyWorkbench.ts
@@ -0,0 +1,61 @@
+import * as AdhConfig from "../../Config/Config";
+import * as AdhMovingColumns from "../../MovingColumns/MovingColumns";
+import * as AdhTopLevelState from "../../TopLevelState/TopLevelState";
+
+export var pkgLocation = "/MyPackage/MyWorkbench";
+
+
+export var workbenchDirective = (
+    adhConfig : AdhConfig.IService,
+    adhTopLevelState : AdhTopLevelState.Service
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/MyWorkbench.html",
+        link: (scope) => {
+            scope.$on("$destroy", adhTopLevelState.bind("view", scope));
+        }
+    };
+};
+
+
+export var proposalCreateColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/ProposalCreateColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl"]);
+        }
+    };
+};
+
+
+export var proposalDetailColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/ProposalDetailColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl", "proposalUrl"]);
+        }
+    };
+};
+
+
+export var proposalListingColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/ProposalListingColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl"]);
+        }
+    };
+};
