8f23c4a31f01ca157284da3aeba7985e12d3d2c2
Joscha Krutzki <joka@jokasis.de>
2016-04-26 18:36:24 +0200

    rm TokenManager

--- src/adhocracy_core/adhocracy_core/scripts/test_delete_stale_login_data.py
+++ src/adhocracy_core/adhocracy_core/scripts/test_delete_stale_login_data.py
@@ -27,23 +27,6 @@ class TestDeleteStaleLoginData:
                             mock)
         return mock
 
-    @fixture
-    def mock_token_manger(self, monkeypatch):
-        from adhocracy_core.authentication import TokenMangerAnnotationStorage
-        from . import delete_stale_login_data
-        mock = Mock(spec=TokenMangerAnnotationStorage)
-        monkeypatch.setattr(delete_stale_login_data, 'get_tokenmanager',
-                            lambda x: mock)
-        return mock
-
-    @fixture
-    def mock_auth_policy(self, registry):
-        from adhocracy_core.authentication import TokenHeaderAuthenticationPolicy
-        mock = Mock(spec=TokenHeaderAuthenticationPolicy)
-        mock.timeout = 0
-        registry.queryUtility = lambda x: mock
-        return mock
-
     def test_delete_stale_users(
             self, context, request_, mock_delete_users, mock_delete_resets):
         self.call_fut(context, request_, 30, 10)
@@ -54,10 +37,3 @@ class TestDeleteStaleLoginData:
             self, context, request_, mock_delete_resets, mock_delete_users):
         self.call_fut(context, request_, 30, 10)
         mock_delete_resets.assert_called_with(request_, 10)
-
-    def test_delete_tokens(
-            self, context, request_, mock_auth_policy, mock_token_manger,
-            mock_delete_users, mock_delete_resets):
-        mock_auth_policy.timeout = 10000
-        self.call_fut(context, request_, 30, 10)
-        mock_token_manger.delete_expired_tokens.assert_called_with(10000)
