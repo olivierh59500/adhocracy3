e2fecdfb0fa706665c2942432eefd68b2a06e212
Joscha Krutzki <joka@jokasis.de>
2016-05-24 17:06:13 +0200

    increase jsamin test timeout for sauclab acceptance test WIP

--- etc/protractorConfSauce.js
+++ etc/protractorConfSauce.js
@@ -49,7 +49,7 @@ exports.config = {
     framework: "jasmine",
     jasmineNodeOpts: {
         showColors: true,
-        defaultTimeoutInterval: 30000,
+        defaultTimeoutInterval: 60000,
         isVerbose: true,
         includeStackTrace: true
     }
